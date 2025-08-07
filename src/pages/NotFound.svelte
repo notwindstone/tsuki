56e187931f0e7f443389e74b7fa2da7519ad6058
notwindstone <notwindstone@gmail.com>
2025-08-07 20:42:29 +0300

    feat: implement proper not found page

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -1,3 +1,11 @@
-<div>
-  Not found
+<!-- if browser supports `svh` (firefox 101+, chrome 108+) -->
+<!-- then page will use `svh` as its max height -->
+<!-- otherwise `vh` will be applied -->
+<div class="flex flex-col gap-2 justify-center items-center max-h-svh h-screen w-full">
+  <p class="font-600 text-4xl">
+    404
+  </p>
+  <p class="font-600 text-2xl">
+    Not Found
+  </p>
 </div>
