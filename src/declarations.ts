f43d391322c2736c389c240b0d7a91485dc2c64e
notwindstone <notwindstone@gmail.com>
2025-08-07 21:10:37 +0300

    refactor: change window declaration structure

--- src/declarations.ts
+++ src/declarations.ts
@@ -2,8 +2,19 @@ declare global {
   // extend `window` to communicate with plugins
   interface Window {
     "__TSUKI__": {
-      // user's search (needed for those plugins that can't use MAL ID)
-      "search": string;
+      // won't change
+      "static": {
+        "appName"  : string;
+        "appRootId": string;
+        "baseUrl"  : string;
+      };
+      // window message event will be fired on change
+      "dynamic": {
+        // user color scheme
+        "theme" : "light" | "dark";
+        // user's search (needed for those plugins that can't use MAL ID)
+        "search": string;
+      };
     };
   }
 }
