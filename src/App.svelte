41163f7af302fd12b8ca5699a478a5c81ebf797b
notwindstone <notwindstone@gmail.com>
2025-08-08 08:53:27 +0300

    feat: configure tanstack query

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import { RouterConfiguration } from "@/constants/routes";
   import { Router } from "@/lib/routing";
   import Layout from "@/Layout.svelte";
+
+  const queryClient = new QueryClient();
 </script>
 
-<Layout>
-  <Router routerConfiguration={RouterConfiguration} />
-</Layout>
+<QueryClientProvider client={queryClient}>
+  <Layout>
+    <Router routerConfiguration={RouterConfiguration} />
+  </Layout>
+</QueryClientProvider>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,11 +2,33 @@
   import { Link, navigate } from "@/lib/routing";
   import Search from "@/components/base/Search.svelte";
   import { useDebounce } from "@/lib/hooks/useDebounce.svelte";
+  import { createQuery } from "@tanstack/svelte-query";
 
   const debouncedSearch = useDebounce("", 300);
+  const query = createQuery({
+    "queryKey": ["todos"],
+    "queryFn" : async () => {
+      const plugin = await fetch("https://raw.githubusercontent.com/notwindstone/anisun-extensions/refs/heads/main/styles-needy-girl-overdose/bundle.js");
+
+      return await plugin.text();
+    },
+  });
 </script>
 
 <div class="flex flex-col gap-2 items-center p-4">
+  {#if $query.isLoading}
+    <p>
+      Loading...
+    </p>
+  {:else if $query.isError}
+    <p>
+      Error: {$query.error.message}
+    </p>
+  {:else if $query.isSuccess}
+    <p class="line-clamp-8 break-all">
+      {JSON.stringify($query.data)}
+    </p>
+  {/if}
   Current: {debouncedSearch.value}
   <Search
     setSearch={debouncedSearch.update}
