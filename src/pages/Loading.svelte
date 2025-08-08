4070b57f1d36854d7464c2713997ea7a61604271
notwindstone <notwindstone@gmail.com>
2025-08-08 08:35:04 +0300

    feat: force loading screen to be dark themed

--- src/pages/Loading.svelte
+++ src/pages/Loading.svelte
@@ -1,8 +1,8 @@
 <!-- if browser supports `svh` (firefox 101+, chrome 108+) -->
 <!-- then page will use `svh` as its max height -->
 <!-- otherwise `vh` will be applied -->
-<div class="flex justify-center items-center max-h-svh h-screen w-full">
-  <p class="font-600 text-4xl">
+<div class="flex justify-center items-center max-h-svh h-screen w-full bg-black">
+  <p class="font-600 text-4xl text-white">
     tsuki.
   </p>
 </div>
