728631c51f3552042cae444c3c0943984293ef3c
notwindstone <notwindstone@gmail.com>
2025-08-10 11:37:53 +0300

    feat: improve Card UI/UX

--- src/components/base/Card.svelte
+++ src/components/base/Card.svelte
@@ -15,33 +15,47 @@
   <Link
     href="/anime"
     params={{ "idMal": entry.idMal }}
-    class="aspect-poster relative flex flex-col justify-between overflow-clip rounded-md"
+    class="group aspect-poster relative flex flex-col justify-between overflow-clip rounded-md"
   >
-    <div class="w-full flex flex-wrap justify-between p-2 text-xs">
-      <div class="rounded-md bg-neutral-100 px-2 py-1 text-black leading-none dark:bg-neutral-900 dark:text-white">
-        FINISHED
-      </div>
-      <div class="rounded-md bg-green-600 px-2 py-1 text-white leading-none">
-        {(entry?.averageScore ?? 0) / 10}
-      </div>
-    </div>
-    <div class="w-full flex flex-col gap-2 p-2">
-      <div class="w-fit rounded-md bg-neutral-900 px-2 py-1 text-xs text-white leading-none dark:bg-neutral-100 dark:text-black">
-        {entry?.currentEpisode} / {entry?.episodes}
-      </div>
-      <p class="text-black dark:text-white">
-        {entry?.title?.english}
-      </p>
-    </div>
     <img
       loading="lazy"
       class={[
-        "absolute h-full w-full object-cover transition-[opacity]",
+        "absolute h-full w-full object-cover transition-[opacity,transform] group-hover:scale-105",
         show ? "opacity-100" : "opacity-0",
       ]}
       alt={`${entry?.title?.english}'s anime cover image`}
       src={entry?.coverImage?.extraLarge}
       onload={() => show = true}
     />
+    <div class="absolute h-full w-full bg-white opacity-50 transition-[opacity] dark:bg-black group-hover:opacity-30"></div>
+    <div class="z-10 w-full flex flex-wrap justify-between gap-2 p-2 text-xs">
+      <!-- data-tooltip styles are defined in globals.css -->
+      <div
+        data-tooltip="FINISHED"
+        data-tooltip-hover="Anime status"
+        class="rounded-md bg-neutral-100 px-2 py-1 text-black leading-none dark:bg-neutral-900 dark:text-white"
+      >
+        <!-- content is displayed by data-tooltip -->
+      </div>
+      <div
+        data-tooltip={(entry?.averageScore ?? 0) / 10}
+        data-tooltip-hover="Others' score"
+        class="rounded-md bg-green-600 px-2 py-1 text-white leading-none"
+      >
+        <!-- content is displayed by data-tooltip -->
+      </div>
+    </div>
+    <div class="z-10 w-full flex flex-col gap-2 p-2">
+      <div
+        data-tooltip={`${entry?.currentEpisode ?? 0} / ${entry?.episodes ?? 0}`}
+        data-tooltip-hover="Episodes watched"
+        class="w-fit rounded-md bg-neutral-900 px-2 py-1 text-xs text-white leading-none dark:bg-neutral-100 dark:text-black"
+      >
+        <!-- content is displayed by data-tooltip -->
+      </div>
+      <p class="line-clamp-3 text-black dark:text-white">
+        {entry?.title?.english}
+      </p>
+    </div>
   </Link>
 {/if}
