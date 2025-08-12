230e7882147069eb65f8561256718b29f2aab04e
notwindstone <notwindstone@gmail.com>
2025-08-12 20:24:50 +0300

    feat: implement a toggle component

---
+++ src/components/base/Toggle.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let {
+    toggled,
+    toggle,
+  }: {
+    "toggled": boolean;
+    "toggle" : () => void;
+  } = $props();
+</script>
+
+<button
+  aria-label="Toggle an option"
+  onclick={toggle}
+  class={[
+    "h-8 w-14 rounded-full p-1 transition-[background-color]",
+    toggled
+      ? "bg-neutral-400 dark:bg-neutral-600"
+      : "bg-neutral-200 dark:bg-neutral-800",
+  ]}
+>
+  <span
+    class={[
+      "block h-6 w-6 rounded-full bg-black dark:bg-white transition-[transform]",
+      toggled ? "translate-x-6" : "translate-x-0",
+    ]}
+  ></span>
+</button>
--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+  import { getCurrentSettingsState } from "@/states/settings/settings.svelte";
+  import Toggle from "@/components/base/Toggle.svelte";
+
+  const settingsState = getCurrentSettingsState().current;
+  const setSettings = getCurrentSettingsState().setSettings;
+</script>
+
 <div class="flex justify-center p-4">
   <div class="max-w-320 w-full">
-    Settings
+    <Toggle
+      toggle={() => setSettings("confirmation", !settingsState.confirmation)}
+      toggled={settingsState.confirmation}
+    />
   </div>
 </div>
