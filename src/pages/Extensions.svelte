35c13732366e0e412396eb5860152329bf40b015
notwindstone <notwindstone@gmail.com>
2025-08-13 21:49:22 +0300

    feat: save code

--- src/pages/Extensions.svelte
+++ src/pages/Extensions.svelte
@@ -2,14 +2,25 @@
   import { Link } from "@/lib/routing/index.js";
   import { getExtensions } from "@/lib/extensions/get-extensions.js";
   import { getCurrentExtensionsState } from "@/states/extensions/extensions.svelte";
-  import { useQueryClient } from "@tanstack/svelte-query";
+  import { createQuery } from "@tanstack/svelte-query";
   import { ExtensionsQueryKey } from "@/constants/extensions";
   import { toggleExtension } from "@/lib/extensions/toggle-extension";
+  import Toggle from "@/components/base/Toggle.svelte";
+  import { removeExtension } from "@/lib/extensions/remove-extension";
+  import { addExtensions } from "@/lib/extensions/add-extensions";
 
-  const queryClient = useQueryClient();
-  const extensions = Object.entries(
-    getExtensions({
-      "local": true,
+  // re-fetch query on state change
+  const extensions = $derived(
+    createQuery({
+      // will be re-fetched on plugin list update
+      "queryKey": ExtensionsQueryKey,
+      "queryFn" : () => {
+        const currentExtensions = getExtensions({
+          "local": true,
+        });
+
+        return Object.entries(currentExtensions);
+      },
     }),
   );
 
@@ -20,16 +31,35 @@
 
 <div class="flex flex-col gap-2 whitespace-pre">
   {JSON.stringify(extensionsState, null, 2)}
-  {#each extensions as extension (extension[0])}
-    <button
-      class="flex"
-      onclick={() => {
-        toggleExtension(extension[0]);
-        queryClient.refetchQueries({ "queryKey": ExtensionsQueryKey });
-      }}
-    >
-      Toggle {extension[0]}
-    </button>
-  {/each}
+  {#if $extensions.data}
+    {#each $extensions.data as extension (extension[0])}
+      <p>
+        Toggle {extension[0]}
+      </p>
+      <Toggle
+        toggled={extension[1]?.enabled === true}
+        toggle={() => {
+          toggleExtension(extension[0]);
+          // re-fetching all extension manifests from local storage on every toggle may be expensive tho
+          $extensions.refetch();
+        }}
+      />
+      <button class="flex" onclick={() => {
+        removeExtension(extension[0]);
+        $extensions.refetch();
+      }}>
+        Remove {extension[0]}
+      </button>
+    {/each}
+  {/if}
+  <button class="flex" onclick={() => {
+    addExtensions({
+      "vue": { "id": "hewwo :3", "logo": "hehe", "name": "lul what~", "url": "https://raw.githubusercontent.com/ame-chan-lol/anisun-vue-example-extension/refs/heads/main/dist/bundle.js", "version": "semver", "authors": [], "languages": [], "categories": [] },
+      "nso": { "id": "ame chan", "logo": "hehe", "name": "lul what~", "url": "https://raw.githubusercontent.com/notwindstone/anisun-extensions/refs/heads/main/styles-needy-girl-overdose/bundle.js", "version": "semver", "authors": [], "languages": [], "categories": [] },
+    });
+    $extensions.refetch();
+  }}>
+    Add some extensions
+  </button>
   <Link href="/">Home</Link>
 </div>
